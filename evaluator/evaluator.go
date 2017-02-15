dce24331b70c8622a5415803cda1ab7ec9c2a93f
Eric Dattore <edattore@gmail.com>
2017-02-15 07:23:54 -0700

    eval integer literals

---
+++ evaluator/evaluator.go
@@ -0,0 +1,31 @@
+package evaluator
+
+import (
+	"monkey/object"
+	"monkey/ast"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
